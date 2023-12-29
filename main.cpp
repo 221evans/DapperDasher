f1293622c6e556b6cd4c254f47f36b289478bc0e
221evans <jayevo95@gmail.com>
2023-12-29 01:46:35 +0000

    Add .gitignore files and generate build files
    This commit adds new .gitignore files to certain directories to prevent unnecessary files from being tracked by git. Additionally, it includes generated build files for the DapperDasher project. This is beneficial for organizing the project structure and ensuring the build process executes correctly.

---
+++ main.cpp
@@ -0,0 +1,62 @@
+#include "raylib.h"
+
+int main()
+{
+
+    // window dimensions
+    const int windowWidth{512};
+    const int windowHeight{380};
+    const int jumpVelocity{-20};
+    bool isJumping{false};
+    InitWindow(windowWidth, windowHeight, "Dapper Dasher");
+    const int gravity{1};
+    SetTargetFPS(60);
+
+    Texture2D scarfy = LoadTexture("/Users/jasonevans/Documents/DapperDasher/textures/scarfy.png");
+    Rectangle scarfyRec;
+    scarfyRec.width = scarfy.width/6;
+    scarfyRec.height = scarfy.height;
+    scarfyRec.x = 0;
+    scarfyRec.y = 0;
+    Vector2 scarfyPos;
+    scarfyPos.x = windowWidth/2 - scarfyRec.width /2;
+    scarfyPos.y = windowHeight - scarfyRec.height;
+
+    // rectangle position
+    int velocity{0};
+
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(WHITE);
+        if (scarfyPos.y >= windowHeight - scarfyRec.height)
+        {
+            // ground check
+            velocity = 0;
+            isJumping = false;
+        }
+        else
+        {
+            // apply gravity
+            velocity += gravity;
+            isJumping = true;
+        }
+
+        if (IsKeyPressed(KEY_SPACE) && !isJumping)
+        {
+
+            velocity += jumpVelocity;
+
+        }
+
+        // apply velocity to position
+        scarfyPos.y += velocity;
+
+        DrawTextureRec(scarfy,scarfyRec,scarfyPos,WHITE);
+
+        EndDrawing();
+    }
+    UnloadTexture(scarfy);
+    CloseWindow();
+}
