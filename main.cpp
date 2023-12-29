65e73e4c17f035d348582d5ea5875fc8e6f7020a
221evans <jayevo95@gmail.com>
2023-12-29 02:17:49 +0000

    Update player physics and animation in game
    Enhanced the game physics to add gravity and improve jump behavior. Added animation frames for the player character to make the movement smoother. Updated build and test log timestamps.

--- main.cpp
+++ main.cpp
@@ -6,11 +6,20 @@ int main()
     // window dimensions
     const int windowWidth{512};
     const int windowHeight{380};
+
+    // player jump variables
     const int jumpVelocity{-600};
     bool isJumping{false};
+    int velocity{0};
+
+    // initiate window
     InitWindow(windowWidth, windowHeight, "Dapper Dasher");
+
+    // add gravity
     const int gravity{1000};
+    // set fps
     SetTargetFPS(60);
+
 
     Texture2D scarfy = LoadTexture("/Users/jasonevans/Documents/DapperDasher/textures/scarfy.png");
     Rectangle scarfyRec;
@@ -22,8 +31,12 @@ int main()
     scarfyPos.x = windowWidth/2 - scarfyRec.width /2;
     scarfyPos.y = windowHeight - scarfyRec.height;
 
-    // rectangle position
-    int velocity{0};
+    // animation frame
+    int frame{};
+    const float updateTime{1.0 / 12.0}; // amount of time before we update animation frame
+    float runningTime{0};
+
+
 
 
     while (!WindowShouldClose())
@@ -54,6 +67,20 @@ int main()
 
         // apply velocity to position
         scarfyPos.y += velocity * deltaTime;
+        //update running time
+        runningTime += deltaTime;
+        if(runningTime >= updateTime)
+        {
+            runningTime = 0;
+            // update animation frame
+            scarfyRec.x = frame * scarfyRec.width;
+            frame ++;
+            if (frame > 5)
+            {
+                frame = 0;
+            }
+        }
+
 
         DrawTextureRec(scarfy,scarfyRec,scarfyPos,WHITE);
 
